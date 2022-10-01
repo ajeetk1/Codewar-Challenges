70d937373ec9320fc4445fde0bce2a823d867ce1
AjeetK <ajeet.khamesra1@outlook.com>
2022-10-01 20:50:58 +0530

    Code Challenges

---
+++ For Twins 1 Types.js
@@ -0,0 +1,12 @@
+// Description:
+//Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+//My Solution:
+
+function typeValidation(variable, type) {
+    if (typeof variable === type){
+      return true;
+    } else {
+      return false;
+    }
+  }
