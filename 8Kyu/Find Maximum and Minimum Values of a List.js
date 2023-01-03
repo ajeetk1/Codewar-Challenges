963a28bec8ebbef7c9b1f4067e4223cfe801a4e6
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-01-03 13:37:15 +0530

    Codewars

--- 8Kyu/Find Maximum and Minimum Values of a List.js
+++ 8Kyu/Find Maximum and Minimum Values of a List.js
@@ -21,3 +21,13 @@ var max = function(list){
 return Math.max(...list)
 }
 
+// Another Method Sort Function 
+var min = function(list){
+    list.sort((a, b) => (a - b));
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a, b) => (b - a));
+    return list[0];
+}
