2aa850324751890267986bdada051a5162d5ff40
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-01-15 18:20:56 +0530

    Codewar Challenges

---
+++ 8Kyu/Localize The Barycenter of a Triangle.js
@@ -0,0 +1,16 @@
+/*8Kyu -Localize The Barycenter of a Triangle.js
+Description - The medians of a triangle are the segments that unit the vertices with the midpoint of their opposite sides. 
+The three medians of a triangle intersect at the same point, called the barycenter or the centroid. Given a triangle, defined by the 
+cartesian coordinates of its vertices we need to localize its barycenter or centroid.
+The function bar_triang() or barTriang or bar-triang, receives the coordinates of the three vertices A, B and C  as three 
+different arguments and outputs the coordinates of the barycenter O in an array [xO, yO]This is how our asked function should work: the result of 
+the coordinates should be expressed up to four decimals, (rounded result).
+You know that the coordinates of the barycenter are given by the following formulas.
+
+My Solution: */
+
+function barTriang(p1, p2, p3){
+    let count1 = +((p1[0] + p2[0] + p3[0]) / 3).toFixed(4);
+    let count2 = +((p1[1] + p2[1] + p3[1]) / 3).toFixed(4);
+      return [count1, count2];
+    }
