1c8151a9105de7831b853aefdc7a0b17fe2d1662
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-01-30 17:01:54 +0530

    8Kyu Codewars

---
+++ 8Kyu/Third Angle of a Triangle.js
@@ -0,0 +1,12 @@
+// Third Angle of a Triangle -8kyu 
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+//My Solution 
+function otherAngle(a, b) {
+  
+    return (180-(a+b));
+  }
