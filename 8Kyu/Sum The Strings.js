33ae1b212428ac56d58118bb8c46a1de138841d3
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-02-01 16:51:06 +0530

    8kyu Challenge

---
+++ 8Kyu/Sum The Strings.js
@@ -0,0 +1,8 @@
+//Sum The Strings 8Kyu 
+//Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+//Example: (Input1, Input2 -->Output)
+
+//My Solution 
+function sumStr(a,b) {
+    return String(Number(a)+Number(b));
+  }
