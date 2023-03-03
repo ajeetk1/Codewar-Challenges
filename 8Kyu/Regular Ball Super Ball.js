77a5a8c36ba8397a06b7d748a6cb5f12e7fe341c
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-03-03 19:37:22 +0530

    Codewars Challenge

---
+++ 8Kyu/Regular Ball Super Ball.js
@@ -0,0 +1,18 @@
+// Regular Ball Super Ball-8kyu 
+//Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+//If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+//ball1 = new Ball();
+//ball2 = new Ball("super");
+
+//ball1.ballType     //=> "regular"
+//ball2.ballType     //=> "super"
+
+// My Solution 
+var Ball = function(ballType) {
+    if (!ballType){
+      this.ballType = 'regular';
+    }else 
+      this.ballType ='super';
+    }
+  
+  
