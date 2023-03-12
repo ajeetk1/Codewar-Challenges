c9c3900aa1eaaf0e4e20a3add3b0af05f6ea2feb
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-03-12 17:04:13 +0530

    Codewar-Challenge

--- 8Kyu/Type of sum.js
+++ 8Kyu/Type of sum.js
@@ -1,5 +1,14 @@
 // Type of sum - 8kyu Description 
 //Return the type of the sum of the two arguments
 
+// My Solution
+function typeOfSum(a, b) {
+  const sum = (a+b);
+  return typeof(sum);
+  }
+
 //My Solution
 const typeOfSum = (a, b) => typeof(a + b);
+
+//-----
+
