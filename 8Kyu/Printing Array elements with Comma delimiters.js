311baaffd6e2ad89d35560b7aa90e90edbcec791
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-03-20 11:18:38 +0530

    Codewar-Challenge

--- 8Kyu/Printing Array elements with Comma delimiters.js
+++ 8Kyu/Printing Array elements with Comma delimiters.js
@@ -1,1 +1,12 @@
 // Printing Array elements with Comma delimiters- Codewars 8kyu 
+// Problem Statement
+/*Input: Array of elements["h","o","l","a"]
+Output: String with comma delimited elements of the array in th same order.
+"h,o,l,a" */
+
+//Solution Steps
+//Remove the "" from the Array we have to use join method as it is used in Array.
+function printArray(array){
+const result = array.join(",") // Remove "" and returns h,o,l,a
+// Now we need to convert to string using toString();
+}
