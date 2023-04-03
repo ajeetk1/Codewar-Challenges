3035d5035733ebccce00a87f0e831b9b976af84f
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-04-03 11:52:32 +0530

    Codewars-Challenges

---
+++ 6Kyu/Remove the parentheses.js
@@ -0,0 +1,13 @@
+// Problem - Remove the parentheses.js
+
+//Description 
+/*Remove the parentheses
+In this kata you are given a string for example:
+"example(unwanted thing)example"
+Your task is to remove everything inside the parentheses as well as the parentheses themselves.
+The example above would return:
+"exampleexample" */
+
+// My Solution
+// First step is to use Regex
+
