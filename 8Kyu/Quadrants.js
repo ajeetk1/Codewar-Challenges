2618352c4aa2bb8b2f7b2da99da592e1d3660320
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-04-21 11:15:28 +0530

    Add Codewar Challenges

--- 8Kyu/Quadrants.js
+++ 8Kyu/Quadrants.js
@@ -3,4 +3,18 @@
 //(integer). x and y are non-zero integers, therefore the given point never lies on the axes.
 
 // My Solution
+//Step :1 I have to add check the numbers(x,y) < && > 0 and put in quadrant 
+
+function quadrant(x, y) {
+    if(x<0 && y>0){
+      return 2;
+    } else if (x<0 && y<0){
+      return 3;
+    } else if (x>0 && y<0){
+      return 4;
+    } else {
+     return 1;   
+      }
+  }  
+    
 
