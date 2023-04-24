e6c91ab73fc448e87894552d6da7049a4b4c253c
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-04-24 12:36:18 +0530

    Codewar-Challenges

--- 7Kyu/Power of two.js
+++ 7Kyu/Power of two.js
@@ -3,3 +3,12 @@
 //a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
 //You may assume the input is always valid.
 
+// My Solution
+ 
+// Here I had to check the option to number is less then 2,equal to 2 and greater then 2.
+
+function isPowerOfTwo(n){
+    if(n<2) return false;
+    if(n==2) return true;
+    if(n>2) return isPowerOfTwo(n/2);
+  }
