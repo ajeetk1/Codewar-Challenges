e8d336f041000286e0b7fa3d4e4c60621f2d94ba
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-05-17 13:29:31 +0530

    Codewars-Challenges

--- 8Kyu/Grasshopper - Summation.js
+++ 8Kyu/Grasshopper - Summation.js
@@ -8,3 +8,13 @@ For example (Input -> Output):
 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8) */
 
 // My Solution 
+
+// Write a  function 
+
+var summation = function (num) {
+    let sum=0;
+    for (let i=1;i<=num;i++){
+      sum=sum+i;
+    }
+    return sum;
+  };
