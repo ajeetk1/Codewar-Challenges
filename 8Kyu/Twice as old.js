c94d48fdaaeb00087c9ce08a7d8f1eb85ccc0198
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-05-20 13:07:12 +0530

    Codewars-Challenges

---
+++ 8Kyu/Twice as old.js
@@ -0,0 +1,15 @@
+// Problem Twice as old - 8kyu Challenges
+/* Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.*/ 
+
+// My Solution 
+
+// Write a function with father years and Son years
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    // We need Absolute Numbers Math.abs 
+    return Math.abs(dadYearsOld - sonYearsOld*2);
+  }
