59c69c6d1cffe45ac794ab25e2a76b17d0a3b6d8
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-05-31 07:09:59 +0530

    Coding Challenge

--- 8Kyu/Sum of positive.js
+++ 8Kyu/Sum of positive.js
@@ -7,4 +7,26 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0. */
 
 // My Solution 
+// Need to Declare a function 
 
+function positivesum(arr){
+// Filter the positive values 
+let positiveArray = arr.filter(function(v){
+    v>0;
+});
+
+// if array is blank
+if(positiveArray ===0){
+return 0;
+}
+
+//loop through the Array.
+else {
+let sum =0;
+for(let i =0; i<=positiveArray.length;i++)
+sum = sum + positiveArray[i];
+}
+
+return sum;
+
+}
